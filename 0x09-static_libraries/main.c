3e9277f98fad0934ed3fef486b8655d46113fb39
Deborah Ogutu <ogutu333@gmail.com>
2022-11-07 09:52:43 -0800

    Task 0

---
+++ 0x09-static_libraries/main.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * main - contain all prototypes
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	_puts("\"At the end of the day, my goal was to be the best hacker\"\n\t
+		- Kevin Mitnick");
+	return (0);
+}
